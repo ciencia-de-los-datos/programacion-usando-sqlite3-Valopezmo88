5a7af73c2caf59a31227f31187fe3cf183a2c662
Valopezmo88 <89362841+Valopezmo88@users.noreply.github.com>
2023-10-16 19:56:15 -0500

    Update pregunta_04.sql

--- pregunta_04.sql
+++ pregunta_04.sql
@@ -31,7 +31,7 @@
 --  Escriba una consulta que retorne los campos K0 y c16
 --  para los registros de la tabla tbl1 para los que la 
 --  columna c16 empieza por la misma letra de la columna K0.
---
+-- 
 --  Rta/
 --    K0   c16
 --  0  E  EGFD
